0db8fc7dc75fe06fe6404a2c78a42a83b9041d0e
Mary <mary.biloshytska@gmail.com>
2022-02-17 21:22:47 +0200

    added animation

--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -2,7 +2,9 @@ import { IUserInfo, ReduxState } from '../../constants/interfaces';
 import { useEffect, useState } from 'react';
 
 import { Image } from 'antd';
+import Lottie from 'react-lottie';
 import { UserInfo } from './components/UserInfo';
+import animationData from '../../assets/animations/11873-click.json';
 import classes from './UserPage.module.css';
 import { getUserData } from '../../utils/api';
 import { useSelector } from 'react-redux';
@@ -10,6 +12,7 @@ import { useSelector } from 'react-redux';
 export const UserPage = () => {
   const [visible, setVisible] = useState(false);
   const [data, setData] = useState<IUserInfo>();
+  const [animating, setAnimating] = useState<boolean>(true);
 
   const {currentUser} = useSelector((state: ReduxState) => state);
 
@@ -29,7 +32,17 @@ export const UserPage = () => {
         src={data?.avatar_url}
         onClick={() => setVisible(true)}
         className={classes.roundImage}
+        onMouseEnter={() => setAnimating(false)}
       />
+      {animating && (
+      <div className={classes.anim}>
+        <Lottie 
+          width={100}
+          height={100}
+          options={{animationData}} 
+          speed={1} />
+      </div>
+    )}
       <div style={{ display: 'none' }}>
         <Image.PreviewGroup preview={{ visible, onVisibleChange: vis => setVisible(vis) }}>
           <Image src={data?.avatar_url} />
